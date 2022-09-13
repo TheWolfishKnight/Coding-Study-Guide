c19dd20181816ab67f96d0ef4b8779f8dc912380
TheWolfishKnight <112997651+TheWolfishKnight@users.noreply.github.com>
2022-09-13 15:44:09 -0400

    Added random topic selector as well as welcome message to console

--- assets/script.js
+++ assets/script.js
@@ -1,24 +1,28 @@
-var topics = ['HTML','CSS','Git', 'Javascript']
-var topic = (undefined)
+var topics = ['HTML','CSS','Git', 'Javascript'];
 
-for(var x = 0; x < topics.length; x++) {
-    console.log (topics[x])
+var randomTopic = topics[Math.floor(Math.random() * topics.length)];
+
+function listTopics() {
+    for(var x = 0; x < topics.length; x++) {
+        console.log (topics[x]);
+}}
 
 
-/*
-}
-if (topic === undefined){
-    console.log ("'Topic' variable empty") 
-} else if (topic === 'CSS'){
-    console.log("CSS makes it pretty, dawg");
-} else if (topic === 'Git'){
-    console.log ("Git is the flow of the code");
-} else if (topic === 'Javascript'){
-    console.log ("Javascript makes the clicks click");
-} else if (topic === 'HTML'){
-    console.log("HTML Learnin' Time");
-}
- else {
+function selectTopic() {
+    if (randomTopic === 'CSS'){
+    console.log("CSS makes it pretty, dawg, let's learn to paint 'sites!");
+}   else if (randomTopic === 'Git'){
+    console.log ("Git is the flow of the code, give into the current");
+}   else if (randomTopic === 'Javascript'){
+    console.log ("Javascript makes the clicks click, get on it!");
+}   else if (randomTopic === 'HTML'){
+    console.log("HTML moves the nation, let's get hyper- marked up in the station.");
+}   else {
     console.log ('What the hell are you saying? Try again fool!');
-}
-*/
+}}
+console.log ("This is what the pre-work went over:");
+
+listTopics()
+
+console.log ("'Aight, check it; this is my recommendation:")
+selectTopic()

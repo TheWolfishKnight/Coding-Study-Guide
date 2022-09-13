c22b5e8ad5591a7a2867469b559578b3a3255284
TheWolfishKnight <112997651+TheWolfishKnight@users.noreply.github.com>
2022-09-13 14:50:51 -0400

    Added javascript capabilities visible in browser console.

---
+++ assets/script.js
@@ -0,0 +1,24 @@
+var topics = ['HTML','CSS','Git', 'Javascript']
+var topic = (undefined)
+
+for(var x = 0; x < topics.length; x++) {
+    console.log (topics[x])
+
+
+/*
+}
+if (topic === undefined){
+    console.log ("'Topic' variable empty") 
+} else if (topic === 'CSS'){
+    console.log("CSS makes it pretty, dawg");
+} else if (topic === 'Git'){
+    console.log ("Git is the flow of the code");
+} else if (topic === 'Javascript'){
+    console.log ("Javascript makes the clicks click");
+} else if (topic === 'HTML'){
+    console.log("HTML Learnin' Time");
+}
+ else {
+    console.log ('What the hell are you saying? Try again fool!');
+}
+*/
